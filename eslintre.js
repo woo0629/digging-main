c721b5944b745e501cf20c87cc9f608f245e2ede
woo0629 <fr.jeong0629@gmail.com>
2024-08-26 19:52:50 +0900

    추가 / eslint 설정

---
+++ eslintre.js
@@ -0,0 +1,9 @@
+module.exports = {
+  extends: ["react-app", "react-app/jest"],
+  rules: {
+    "no-unused-vars": "warn",
+    "react-hooks/exhaustive-deps": "warn",
+    "jsx-a11y/alt-text": "warn",
+    "jsx-a11y/anchor-is-valid": "warn",
+  },
+};
